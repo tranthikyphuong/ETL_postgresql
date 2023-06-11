901c340ce7afb82ca76b3ffe0416c6cfed151074
Trần Kỳ Phương <86771179+tranthikyphuong@users.noreply.github.com>
2023-06-11 14:12:33 +0700

    Update create_table.py

--- create_table.py
+++ create_table.py
@@ -3,12 +3,11 @@ from sql_queries import create_table_queries, drop_table_queries
 
 
 def create_database():
-    # connect to default database
     conn = psycopg2.connect("host=127.0.0.1 dbname=meudb user=postgres password=xxxxx")
     conn.set_session(autocommit=True)
     cur = conn.cursor()
 
-    # create sparkify database with UTF8 encoding
+
     cur.execute("DROP DATABASE IF EXISTS sparkifydb")
     cur.execute("CREATE DATABASE sparkifydb WITH ENCODING 'utf8' TEMPLATE template0")
 
